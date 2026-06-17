c72164b3d2c743dc469b3a7e5dfbf45a5eb530f9
Sabylie <0001253328@senaimgaluno.com.br>
2026-06-17 19:35:29 -0300

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,8 @@
+let idade = Number (prompt("Qual é a sua idade"));
+if (idade > 18) {
+ console.log ("Acesso liberado:Maior de idade");
+} else if (idade > 18) {
+ console.log ("Acesso negado:Menor de idade");
+} else {
+ console.log ("Acesso de idade");
+}
