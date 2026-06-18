4993c26ad1de9bd96215bd147c2150d076cc1b88
Sabylie <0001253328@senaimgaluno.com.br>
2026-06-18 15:22:26 -0300

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,8 @@
+let número = Number(prompt("Digite um  numero"));
+
+ if (número%2 === 0) {
+  console.log ("número é um número par");
+}
+ else {
+  console.log ("número é um número impar");
+}
---
+++ 4.js
@@ -0,0 +1,11 @@
+let número = Number (prompt("Digite um número: "));
+let número2 = Number (prompt("Digite um número: "));
+
+if (número > número2){
+    alert("número2 é o maior número")
+}
+else{
+    alert ("número é o maior número")
+}
+
+
