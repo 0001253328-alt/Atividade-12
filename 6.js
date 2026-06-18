2ba4928428552e10124ee91b8257423d09c476bb
Sabylie <0001253328@senaimgaluno.com.br>
2026-06-18 16:02:28 -0300

    Add files via upload

---
+++ 6.js
@@ -0,0 +1,9 @@
+let idade = Number (prompt ("Qual é a sua idade?"));
+let estudante = (prompt ("Você éestudante" ));
+
+if ((estudante === "sim")||(idade > 60)){
+    console.log ("Você tem direito de meia entrada")
+}
+ else {
+   console.log ("Você não tem direito de meia entrada")
+ }
