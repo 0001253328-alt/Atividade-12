04034ceee4214ab7b99c0599da4f9f8ee158b79d
Sabylie <0001253328@senaimgaluno.com.br>
2026-06-18 14:00:00 -0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite um número:"));
+
+if (numero > 0) {
+    console.log("O número é Positivo.");
+} else if (numero < 0) {
+    console.log("O número é Negativo.");
+} else {
+    console.log("O número é Exatamente Zero.");
+}
